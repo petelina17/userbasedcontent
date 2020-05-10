9b2452e3781c3b6b937ae1eac31854cff41171d6
petelina17 <petelina17@gmail.com>
2020-05-10 23:09:27 +0200

    react + express (check README.md)

--- client/src/components/homePage/homepage.js
+++ client/src/components/homePage/homepage.js
@@ -4,9 +4,25 @@ import { Link } from "react-router-dom";
 import { Button } from "@material-ui/core";
 
 class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: '' };
+  }
+
+  callAPI() {
+    fetch('http://localhost:9000/testAPI')
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
   render() {
     return (
       <div className="homepage-box">
+        <p className="App-intro">{this.state.apiResponse}</p>
         <div>INSERT LOGO</div>
         <h5>Create an account or log in.</h5>
         <Link to="/login">
