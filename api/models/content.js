d27a6d8fdc80d36196242bf2cac9dd804b6eefc2
Mattin <mattin-najafi@hotmail.com>
2020-05-14 17:30:32 +0200

    edit and deletebutton based on whos logged in

--- api/models/content.js
+++ api/models/content.js
@@ -1,14 +1,14 @@
-var mongoose = require('mongoose')
+var mongoose = require("mongoose");
 
 // Schema
 const contentSchema = new mongoose.Schema({
   title: String,
   username: String,
+  text: String,
   date: String,
-  text: String
 });
 
 // Model
-const Content = mongoose.model('Content', contentSchema);
+const Content = mongoose.model("Content", contentSchema);
 
-module.exports = Content
+module.exports = Content;
--- client/src/components/content/content.jsx
+++ client/src/components/content/content.jsx
@@ -11,13 +11,117 @@ class Content extends React.Component {
     title: "",
   };
 
+  componentDidMount() {
+    console.log(this.props.location.state);
+    fetch("http://localhost:9000/content", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((res) => res.json())
+      .catch((err) => {
+        console.log("[ERROR]", err);
+      })
+      .then((res) => {
+        console.log("get response: ", res);
+        for (let i = 0; i < res.length; i++) {
+          if (this.props.location.state === res[i].username) {
+            let postDiv = document.createElement("div");
+            postDiv.id = `post${id++}`;
+            postDiv.className = "post";
+            postDiv.innerHTML = `<div class="titleDiv"><h2> ${res[i].title}</h2>
+                              <div class='delete-edit-div'>
+                              <button class='delete'> X </button>
+                              <button class='edit'> EDIT </button>
+                              </div>
+                              </div>
+                              <div>
+                              <span> ${res[i].username} </span>
+                              <span> ${res[i].date}</span>
+                              </div>
+                              <p>${res[i].text}</p>
+                              `;
+            document.getElementById("allContent").appendChild(postDiv);
+          } else {
+            let postDiv = document.createElement("div");
+            postDiv.id = `post${id++}`;
+            postDiv.className = "post";
+            postDiv.innerHTML = `<div class="titleDiv"><h2> ${res[i].title}</h2>
+                              </div>
+                              <div>
+                              <span> ${res[i].username} </span>
+                              <span> ${res[i].date}</span>
+                              </div>
+                              <p>${res[i].text}</p>
+                              `;
+            document.getElementById("allContent").appendChild(postDiv);
+          }
+        }
+        document.querySelectorAll(".delete").forEach((item) => {
+          item.addEventListener("click", this.deletePost);
+        });
+        document.querySelectorAll(".edit").forEach((item) => {
+          item.addEventListener("click", this.editPost);
+        });
+      });
+  }
+  editPost = (e) => {
+    let postDiv = e.toElement.parentElement.parentElement.parentElement;
+    let title = e.toElement.parentElement.parentElement.firstChild.innerText;
+    let username =
+      e.toElement.parentElement.parentElement.nextSibling.nextSibling
+        .children[0].innerText;
+    let date =
+      e.toElement.parentElement.parentElement.nextSibling.nextSibling
+        .children[1].innerText;
+    let post =
+      e.toElement.parentElement.parentElement.nextSibling.nextSibling
+        .nextSibling.nextSibling.innerText;
+    //   fetch("http://localhost:9000/content", {
+    //     method: "PUT",
+    //     headers: {
+    //       "Content-Type": "application/json; charset=UTF-8",
+    //     },
+    //   })
+    //     .then((res) => res.json())
+    //     .catch((err) => {
+    //       console.log("[ERROR]", err);
+    //     })
+    //     .then((res) => {
+    //       console.log("delete response: ", res);
+    //     });
+    //   console.log(e);
+  };
   deletePost = (e) => {
-    // CODE FOR DELETE POST
-    console.log(e);
+    let postDiv = e.toElement.parentElement.parentElement.parentElement;
+    let title = e.toElement.parentElement.parentElement.firstChild.innerText;
+    let username =
+      e.toElement.parentElement.parentElement.nextSibling.nextSibling
+        .children[0].innerText;
+    let date =
+      e.toElement.parentElement.parentElement.nextSibling.nextSibling
+        .children[1].innerText;
+    let post =
+      e.toElement.parentElement.parentElement.nextSibling.nextSibling
+        .nextSibling.nextSibling.innerText;
+    console.log(post);
+    //   fetch("http://localhost:9000/content", {
+    //     method: "DELETE",
+    //     headers: {
+    //       "Content-Type": "application/json; charset=UTF-8",
+    //     },
+    //   })
+    //     .then((res) => res.json())
+    //     .catch((err) => {
+    //       console.log("[ERROR]", err);
+    //     })
+    //     .then((res) => {
+    //       console.log("delete response: ", res);
+    //     });
+    //   console.log(e);
   };
-  addPost = () => {
-    this.setState({ addPost: true });
-  };
+
   createdPost = () => {
     let date = new Date();
     let year = date.getFullYear();
@@ -32,21 +136,25 @@ class Content extends React.Component {
     postDiv.id = `post${id++}`;
     postDiv.className = "post";
     postDiv.innerHTML = `<div class="titleDiv"><h2> ${this.state.title}</h2>
-    <button class='click'> X </button>
+    <div class='delete-edit-div'>
+    <button class='delete'> X </button>
+    <button class='edit'> EDIT </button>
+    </div>
     </div>
     <div>
     <span> ${this.props.location.state} </span>
     <span> ${year}-${month}-${day}</span>
     </div>
-    <p>${this.state.post}</p>
+    <p>${this.state.text}</p>
     `;
 
     let post = {
       title: this.state.title,
+      username: this.props.location.state,
       text: this.state.text,
       date: today,
-      username: this.props.location.state,
     };
+
     fetch("http://localhost:9000/content", {
       method: "POST",
       headers: {
@@ -72,48 +180,10 @@ class Content extends React.Component {
     this.setState({ title: "" });
   };
 
-  componentDidMount() {
-    fetch("http://localhost:9000/content", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((res) => res.json())
-      .catch((err) => {
-        console.log("[ERROR]", err);
-      })
-      .then((res) => {
-        console.log("get response: ", res);
-        for (let i = 0; i < res.length; i++) {
-          console.log(res[i]);
-          let postDiv = document.createElement("div");
-          postDiv.id = `post${id++}`;
-          postDiv.className = "post";
-          postDiv.innerHTML = `<div class="titleDiv"><h2> ${res[i].title}</h2>
-                              <button class='click'> X </button>
-                              </div>
-                              <div>
-                              <span> ${res[i].username} </span>
-                              <span> ${res[i].date}</span>
-                              </div>
-                              <p>${res[i].date}</p>
-                              `;
-          document.getElementById("allContent").appendChild(postDiv);
-        }
-      });
-  }
-
-  componentDidUpdate() {
-    let click = document.querySelector(".click");
-    if (click) {
-      click.addEventListener("click", this.deletePost);
-    }
-  }
-
   logOut = () => {
     this.props.history.push("/");
   };
+
   render() {
     return (
       <div id="content-div">
@@ -124,7 +194,12 @@ class Content extends React.Component {
           <main id="allContent"></main>
         </div>
         <div className="buttondiv">
-          <Button variant="contained" onClick={this.addPost}>
+          <Button
+            variant="contained"
+            onClick={() => {
+              this.setState({ addPost: true });
+            }}
+          >
             Add post
           </Button>
           <Button variant="contained" onClick={this.logOut}>
@@ -146,9 +221,9 @@ class Content extends React.Component {
               />
               <h1>Post</h1>
               <textarea
-                value={this.state.post}
+                value={this.state.text}
                 onChange={(event) =>
-                  this.setState({ post: event.target.value })
+                  this.setState({ text: event.target.value })
                 }
                 rows="6"
                 column="150"
