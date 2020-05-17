e7ff29effb4d6c9c6abf53eed6763fe76e73770b
Mattin <mattin-najafi@hotmail.com>
2020-05-17 10:32:32 +0200

    Updates

---
+++ precache-manifest.64c71c79b4819c7be624067e78b444c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5450806f94cde5f59c9f0b3031a4700e",
+    "url": "/userbasedcontent/index.html"
+  },
+  {
+    "revision": "2023f1c0f2fd8cecf626",
+    "url": "/userbasedcontent/static/css/main.0db4e3a9.chunk.css"
+  },
+  {
+    "revision": "7a95f3dcd804053f57ab",
+    "url": "/userbasedcontent/static/js/2.7f5724f0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/userbasedcontent/static/js/2.7f5724f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2023f1c0f2fd8cecf626",
+    "url": "/userbasedcontent/static/js/main.824660b0.chunk.js"
+  },
+  {
+    "revision": "1340377d3a77788319e7",
+    "url": "/userbasedcontent/static/js/runtime-main.42f375fd.js"
+  }
+]);
