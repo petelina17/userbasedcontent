62668aaf05f1baf2f7af86efefb7679581dafc19
petelina17 <petelina17@gmail.com>
2020-06-13 22:09:42 +0200

    content changed to react functional component

--- client/src/components/content/content.jsx
+++ client/src/components/content/content.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 import './content.css'
 import ForumIcon from '@material-ui/icons/Forum'
 import {Button} from '@material-ui/core'
@@ -8,37 +8,39 @@ import HomePage from '../homePage/homepage'
 
 let id = 0
 
-class Content extends React.Component {
-  static contextType = StateContext
-
-  state = {
+export const Content = (props) => {
+  const context = useContext(StateContext)
+  const [state, setState] = useState({
     addPost: false,
     edit: false,
     text: '',
     title: '',
     blogPosts: [],
-    username: 'unknown',
-  }
+    // username: null,
+  })
 
-  componentDidMount() {
-    if (this.context.username == null) {
+  useEffect(() => {
+    if (context.username == null) {
       const currentUser = localStorage.getItem('ubc.username')
       if (currentUser != null) {
-        this.context.username = currentUser
+        context.username = currentUser
       }
     }
-    console.log('username:', this.context.username)
-    this.setState({username: this.context.username})
-    this.getContent()
-  }
+    console.log('username 1:', context.username)
 
-  forgetUser = () => {
-    this.context.username = null
-    this.setState({username: null})
+    setState({...state, username: context.username})
+    console.log('username 2:', context.username)
+    getContent()
+
+  }, [])
+
+  let forgetUser = () => {
+    context.username = null
+    setState({...state, username: null})
     localStorage.removeItem('ubc.username')
   }
 
-  getContent = () => {
+  let getContent = () => {
     fetch('http://localhost:9000/contents', {
       headers: {
         'Content-Type': 'application/json; charset=UTF-8',
@@ -62,34 +64,36 @@ class Content extends React.Component {
               date: content.date,
               text: content.text,
               currentId: undefined,
-              readOnly: content.username == null || content.username === '' || this.state.username !== content.username,
+              readOnly: content.username == null || content.username === '' || context.username !== content.username,
             }
             collection.push(blogPost)
           })
-          this.setState({blogPosts: collection})
+          setState({...state, blogPosts: collection})
         })
   }
 
-  getIdOfPost = (e, id) => {
-    let blogPost = this.state.blogPosts.find((x) => x.postId === id)
+  let getIdOfPost = (e, id) => {
+    let blogPost = state.blogPosts.find((x) => x.postId === id)
     if (blogPost == null) {
       console.log('[ERROR] blogPost is null, id:', id)
       return
     }
 
-    this.props.history.push(`/content/${blogPost._id}`)
+    props.history.push(`/content/${blogPost._id}`)
     e.preventDefault()
 
-    this.setState({addPost: true})
-    this.setState({
+    setState({
+      ...state,
+      addPost: true,
       edit: true,
       text: blogPost.text,
       title: blogPost.title,
-      currentId: blogPost._id,
+      currentId: blogPost._id
     })
+
   }
 
-  editPost = (e) => {
+  let editPost = (e) => {
     let date = new Date()
     let year = date.getFullYear()
     let month = date.getMonth() + 1
@@ -99,14 +103,14 @@ class Content extends React.Component {
     if (day < 10) day = '0' + day
 
     let editedPost = {
-      title: this.state.title,
-      username: this.state.username,
-      text: this.state.text,
+      title: state.title,
+      username: context.username,
+      text: state.text,
       date: today,
     }
-    const temp = JSON.parse(JSON.stringify(this.state))
+    const temp = JSON.parse(JSON.stringify(state))
 
-    fetch(`http://localhost:9000/content/${this.state.currentId}`, {
+    fetch(`http://localhost:9000/content/${state.currentId}`, {
       method: 'PUT',
       credentials: 'include',
       headers: {
@@ -119,7 +123,7 @@ class Content extends React.Component {
           console.log('[ERROR]', err)
         })
         .then((res) => {
-          const found = this.state.blogPosts.find(
+          const found = state.blogPosts.find(
               (x) => x._id === temp.currentId
           )
           // console.log('after edit, found blog post:', found)
@@ -127,15 +131,13 @@ class Content extends React.Component {
             found.text = temp.text
             found.title = temp.title
           }
-          this.props.history.push('/content')
+          props.history.push('/content')
           console.log('edit response: ', res)
         })
-
-    this.setState({addPost: false})
-    this.setState({edit: false, title: '', text: '', currentId: undefined})
+    setState({...state, addPost: false, edit: false, title: '', text: '', currentId: undefined})
   }
 
-  deletePost = (e, id) => {
+  let deletePost = (e, id) => {
     fetch(`http://localhost:9000/content/${id}`, {
       method: 'DELETE',
       credentials: 'include',
@@ -147,15 +149,16 @@ class Content extends React.Component {
         .catch((err) => {
           console.log('[ERROR]', err)
         })
+
         .then((res) => {
-          let collection = [...this.state.blogPosts.filter((x) => x._id !== id)]
-          this.setState({blogPosts: [...collection]})
+          let collection = [...state.blogPosts.filter((x) => x._id !== id)]
+          setState({...state, blogPosts: [...collection]})
           console.log('delete response: ', res)
           // console.log(e.toElement.parentElement.parentElement.parentElement);
         })
   }
 
-  createdPost = () => {
+  let createdPost = () => {
     let date = new Date()
     let year = date.getFullYear()
     let month = date.getMonth() + 1
@@ -168,23 +171,22 @@ class Content extends React.Component {
     let postDiv = document.createElement('div')
     postDiv.id = `post${id++}`
     postDiv.className = 'post'
-    postDiv.innerHTML = `<div class="titleDiv"><h2> ${this.state.title}</h2>
+    postDiv.innerHTML = `<div class="titleDiv"><h2> ${state.title}</h2>
     <div class='delete-edit-div'>
     <button class='delete'> X </button>
     <button class='edit'> EDIT </button>
     </div>
     </div>
     <div>
-    <span> ${this.state.username} </span>
+    <span> ${context.username} </span>
     <span> ${year}-${month}-${day}</span>
     </div>
-    <p>${this.state.text}</p>
+    <p>${state.text}</p>
     `
-
     let post = {
-      title: this.state.title,
-      username: this.state.username,
-      text: this.state.text,
+      title: state.title,
+      username: context.username,
+      text: state.text,
       date: today,
     }
 
@@ -204,15 +206,16 @@ class Content extends React.Component {
           console.log('POST content response: ', res)
           if (res.createPost === true) {
             console.log('blog post added:', res.blogPost)
-            const collection = this.state.blogPosts
+            const collection = state.blogPosts
             collection.push(res.blogPost)
-            this.setState({blogPosts: collection})
+            setState({...state, blogPosts: collection, addPost: false, post: '', title: ''})
 
             console.log('createPost is true')
-            this.setState({addPost: false})
+            // setState({...state, addPost: false})
             // document.getElementById('allContent').appendChild(postDiv)
-            this.setState({post: ''})
-            this.setState({title: ''})
+            // setState({...state, post: ''})
+            // setState({...state, title: ''})
+            return
           }
           console.log('createPost is not True')
         })
@@ -221,8 +224,8 @@ class Content extends React.Component {
         })
   }
 
-  logout = async () => {
-    this.forgetUser()
+  let logout = async () => {
+    forgetUser()
     await fetch('http://localhost:9000/logout', {
       credentials: 'include',
       method: 'POST',
@@ -231,168 +234,166 @@ class Content extends React.Component {
         .then(res => res.json())
         .then((res) => {
           console.log('logout response: ', res)
-          // this.props.history.push("/");
+          // props.history.push("/");
         })
         .catch((err) => {
           console.log('[ERROR]', err)
         })
         .finally(() => {
-          this.getContent()
+          getContent()
         })
   }
 
+  const logoutButton = <Button id="homepageButton" variant="contained" onClick={() => {
+    logout()
+  }}>
+    Log out
+  </Button>
 
-
-  render() {
-    const logoutButton = <Button id="homepageButton" variant="contained" onClick={() => { this.logout() }}>
-      Log out
-    </Button>
-
-    return (
-        <div id="content-div">
-          <div>
-            <header id="header">
-              <div
-                  style={{width: '13rem', display: 'flex', alignItems: 'center'}}
+  return (
+      <div id="content-div">
+        <div>
+          <header id="header">
+            <div
+                style={{width: '13rem', display: 'flex', alignItems: 'center'}}
+            >
+              <ForumIcon color="secondary" style={{fontSize: '40'}}/>
+              <span
+                  style={{
+                    fontSize: '3rem',
+                    fontWeight: '800',
+                    marginLeft: '0.4rem',
+                  }}
               >
-                <ForumIcon color="secondary" style={{fontSize: '40'}}/>
-                <span
-                    style={{
-                      fontSize: '3rem',
-                      fontWeight: '800',
-                      marginLeft: '0.4rem',
-                    }}
-                >
                 <img src={logo} alt="Logo" style={{height: '2.1rem'}}/>
               </span>
-              </div>
-              { this.state.username == null ? <HomePage /> : logoutButton}
-            </header>
+            </div>
+            {context.username == null ? <HomePage/> : logoutButton}
+          </header>
 
-            <main id="allContent">
-              {this.state.blogPosts.map((blogPost) => (
-                  <div id={blogPost.postId} className="post" key={blogPost.postId}>
-                    <div className="titleDiv">
-                      <h2>{blogPost.title}</h2>
+          <main id="allContent">
+            {state.blogPosts.map((blogPost) => (
+                <div id={blogPost.postId} className="post" key={blogPost.postId}>
+                  <div className="titleDiv">
+                    <h2>{blogPost.title}</h2>
 
-                      {!blogPost.readOnly ? (
-                          <div className="delete-edit-div">
-                            <button
-                                className="delete"
-                                onClick={(e) => {
-                                  this.deletePost(e, blogPost._id)
-                                }}
-                            >
-                              X
-                            </button>
-                            <button
-                                className="edit"
-                                onClick={(e) => {
-                                  this.getIdOfPost(e, blogPost.postId)
-                                }}
-                            >
-                              EDIT
-                            </button>
-                          </div>
-                      ) : (
-                          ''
-                      )}
-                    </div>
-                    <div>
-                      <span>{blogPost.username}</span>
-                      <span>{blogPost.date}</span>
-                    </div>
-                    <p>{blogPost.text}</p>
+                    {!blogPost.readOnly ? (
+                        <div className="delete-edit-div">
+                          <button
+                              className="delete"
+                              onClick={(e) => {
+                                deletePost(e, blogPost._id)
+                              }}
+                          >
+                            X
+                          </button>
+                          <button
+                              className="edit"
+                              onClick={(e) => {
+                                getIdOfPost(e, blogPost.postId)
+                              }}
+                          >
+                            EDIT
+                          </button>
+                        </div>
+                    ) : (
+                        ''
+                    )}
                   </div>
-              ))}
-            </main>
-          </div>
-
-          {this.state.username ? (
-              <div className="buttondiv">
-                <Button variant="contained" color="secondary"
-                        onClick={() => {
-                          this.setState({...this.state, addPost: true, title: '', text: '', edit: false})
-                        }}
-                >
-                  Add post
-                </Button>
-              </div>
-          ) : ''}
-
-          {this.state.addPost === true ? (
-              <React.Fragment>
-                <div className="createPost-div">
-                  <h1>Title</h1>
-                  <input
-                      defaultValue={this.state.title}
-                      onChange={(event) =>
-                          this.setState({title: event.target.value})
-                      }
-                      type="text"
-                      placeholder="Enter title here..."
-                  />
-                  <h1>Post</h1>
-                  <textarea
-                      defaultValue={this.state.text}
-                      onChange={(event) =>
-                          this.setState({text: event.target.value})
-                      }
-                      rows="6"
-                      column="150"
-                      placeholder="Enter post here..."
-                  ></textarea>
-                  {this.state.edit === false ? (
-                      <React.Fragment>
-                        <Button
-                            variant="contained"
-                            color="secondary"
-                            onClick={this.createdPost}
-                            id="createEditPost"
-                        >
-                          Create Post
-                        </Button>
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            onClick={() => {
-                              this.setState({addPost: false})
-                            }}
-                            id="cancelPost"
-                        >
-                          Cancel Post
-                        </Button>
-                      </React.Fragment>
-                  ) : (
-                      <React.Fragment>
-                        <Button
-                            variant="contained"
-                            color="secondary"
-                            onClick={this.editPost}
-                            id="createEditPost"
-                        >
-                          Edit Post
-                        </Button>
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            onClick={() => {
-                              this.setState({addPost: false})
-                            }}
-                            id="cancelPost"
-                        >
-                          Cancel Post
-                        </Button>
-                      </React.Fragment>
-                  )}
+                  <div>
+                    <span>{blogPost.username}</span>
+                    <span>{blogPost.date}</span>
+                  </div>
+                  <p>{blogPost.text}</p>
                 </div>
-              </React.Fragment>
-          ) : (
-              <React.Fragment></React.Fragment>
-          )}
+            ))}
+          </main>
         </div>
-    )
-  }
+
+        {context.username ? (
+            <div className="buttondiv">
+              <Button variant="contained" color="secondary"
+                      onClick={() => {
+                        setState({...state, addPost: true, title: '', text: '', edit: false})
+                      }}
+              >
+                Add post
+              </Button>
+            </div>
+        ) : ''}
+
+        {state.addPost === true ? (
+            <React.Fragment>
+              <div className="createPost-div">
+                <h1>Title</h1>
+                <input
+                    defaultValue={state.title}
+                    onChange={(event) =>
+                        setState({...state, title: event.target.value})
+                    }
+                    type="text"
+                    placeholder="Enter title here..."
+                />
+                <h1>Post</h1>
+                <textarea
+                    defaultValue={state.text}
+                    onChange={(event) =>
+                        setState({...state, text: event.target.value})
+                    }
+                    rows="6"
+                    column="150"
+                    placeholder="Enter post here..."
+                />
+                {state.edit === false ? (
+                    <React.Fragment>
+                      <Button
+                          variant="contained"
+                          color="secondary"
+                          onClick={createdPost}
+                          id="createEditPost"
+                      >
+                        Create Post
+                      </Button>
+                      <Button
+                          variant="contained"
+                          color="primary"
+                          onClick={() => {
+                            setState({...state, addPost: false})
+                          }}
+                          id="cancelPost"
+                      >
+                        Cancel Post
+                      </Button>
+                    </React.Fragment>
+                ) : (
+                    <React.Fragment>
+                      <Button
+                          variant="contained"
+                          color="secondary"
+                          onClick={editPost}
+                          id="createEditPost"
+                      >
+                        Edit Post
+                      </Button>
+                      <Button
+                          variant="contained"
+                          color="primary"
+                          onClick={() => {
+                            setState({...state, addPost: false})
+                          }}
+                          id="cancelPost"
+                      >
+                        Cancel Post
+                      </Button>
+                    </React.Fragment>
+                )}
+              </div>
+            </React.Fragment>
+        ) : (
+            <React.Fragment/>
+        )}
+      </div>
+  )
 }
 
 export default Content
--- client/src/components/loginPage/loginpage.jsx
+++ client/src/components/loginPage/loginpage.jsx
@@ -1,13 +1,15 @@
-import * as React from "react";
-import { Link } from "react-router-dom";
-import { TextField, Button } from "@material-ui/core";
-import ExitToAppIcon from "@material-ui/icons/ExitToApp";
-import "./loginpage.css";
-import logo from "./logo.png";
+import * as React from 'react'
+import {Link} from 'react-router-dom'
+import {TextField, Button} from '@material-ui/core'
+import ExitToAppIcon from '@material-ui/icons/ExitToApp'
+import './loginpage.css'
+import logo from './logo.png'
 import StateContext from '../../contexts/StateContext'
+import {useContext, useState} from 'react'
+import user from '../../contexts/StateContext'
 
 export const rememberUser = (username, context) => {
-  context.username = username;
+  context.username = username
   localStorage.setItem('ubc.username', username)
 }
 
@@ -15,112 +17,110 @@ export const checkLogin = (username, password, history, context) => {
   const data = {
     username: username,
     password: password,
-  };
+  }
 
-  fetch("http://localhost:9000/login", {
-    method: "POST",
+  fetch('http://localhost:9000/login', {
+    method: 'POST',
     credentials: 'include',
     headers: {
-      "Content-type": "application/json; charset=UTF-8", // Indicates the content
+      'Content-type': 'application/json; charset=UTF-8', // Indicates the content
     },
     body: JSON.stringify(data),
   })
       .then((res) => res.json())
       .catch((err) => {
-        console.log("[ERROR]", err);
+        console.log('[ERROR]', err)
       })
       .then((res) => {
-        console.log("login response: ", res);
+        console.log('login response: ', res)
         if (res.login === true) {
           rememberUser(data.username, context)
-          history.push("/content");
+          history.push('/content')
         }
-      });
-};
+      })
+}
 
-class LoginPage extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      username: "",
-      password: "",
-      isLogged: false,
-    };
-  }
+export const LoginPage = (props) => {
+  const context = useContext(StateContext)
+  const [state, setState] = useState({
+    username: '',
+    password: '',
+    isLogged: false,
+  })
 
-  static contextType = StateContext
+  // static contextType = StateContext
 
-  render () {
+  {
     return (
-      <div className="login-box">
-        <div style={{ width: "13rem", display: "flex", alignItems: "center" }}>
-          <ExitToAppIcon fontSize="large" color="secondary" />
-          <span
-            style={{
-              fontSize: "3rem",
-              fontWeight: "800",
-              marginLeft: "0.4rem",
-            }}
-          >
-            <img src={logo} alt="Logo" style={{ height: "2.1rem" }} />
+        <div className="login-box">
+          <div style={{width: '13rem', display: 'flex', alignItems: 'center'}}>
+            <ExitToAppIcon fontSize="large" color="secondary"/>
+            <span
+                style={{
+                  fontSize: '3rem',
+                  fontWeight: '800',
+                  marginLeft: '0.4rem',
+                }}
+            >
+            <img src={logo} alt="Logo" style={{height: '2.1rem'}}/>
           </span>
+          </div>
+
+          {context.userCreated === true ? (
+              <div className="usercreated-div">
+                Your user was successfully created.
+              </div>
+          ) : (
+              <></>
+          )}
+
+          {state.isLogged === true ? (
+              <div className="notLoggedText-div">
+                Invalid username or email.
+                <br/> Try again
+              </div>
+          ) : (
+              <></>
+          )}
+
+          <TextField
+              size="small"
+              label="Username"
+              variant="outlined"
+              required
+              type="text"
+              value={state.username}
+              onChange={(event) => {
+                setState({...state, username: event.target.value})
+              }}
+          />
+          <TextField
+              size="small"
+              label="Password"
+              variant="outlined"
+              required
+              type="password"
+              onChange={(event) => {
+                setState({...state, password: event.target.value})
+              }}
+          />
+          <Button
+              size="medium"
+              id="login-btn"
+              variant="contained"
+              color="secondary"
+              onClick={() => {
+                checkLogin(state.username, state.password, props.history, context)
+              }}
+          >
+            LOGIN
+          </Button>
+          <p>
+            No account? <Link to="/register">Register</Link>
+          </p>
         </div>
-
-        {this.context.userCreated === true ? (
-          <div className="usercreated-div">
-            Your user was successfully created.
-          </div>
-        ) : (
-          <></>
-        )}
-
-        {this.state.isLogged === true ? (
-          <div className="notLoggedText-div">
-            Invalid username or email.
-            <br /> Try again
-          </div>
-        ) : (
-          <></>
-        )}
-
-        <TextField
-          size="small"
-          label="Username"
-          variant="outlined"
-          required
-          type="text"
-          value={this.state.username}
-          onChange={(event) => {
-            this.setState({ username: event.target.value });
-          }}
-        />
-        <TextField
-          size="small"
-          label="Password"
-          variant="outlined"
-          required
-          type="password"
-          onChange={(event) => {
-            this.setState({ password: event.target.value });
-          }}
-        />
-        <Button
-          size="medium"
-          id="login-btn"
-          variant="contained"
-          color="secondary"
-          onClick={() => {
-            checkLogin(this.state.username, this.state.password, this.props.history, this.context)
-          }}
-        >
-          LOGIN
-        </Button>
-        <p>
-          No account? <Link to="/register">Register</Link>
-        </p>
-      </div>
-    );
+    )
   }
 }
 
-export default LoginPage;
+export default LoginPage
